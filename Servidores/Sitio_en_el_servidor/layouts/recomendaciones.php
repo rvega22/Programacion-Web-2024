df714e447b3571b75fec76acd1044ef2b719f4a1
Roberto <rvega_22@alu.uabcs.mx>
2024-11-28 21:56:59 -0700

    header

---
+++ Servidores/Sitio_en_el_servidor/layouts/recomendaciones.php
@@ -0,0 +1,57 @@
+<nav class="centrar_nav">
+            <div class="target">
+                <a href="venta.html">
+                    <img class="target_img centrar_img" src="img/Imagen_lentes_2.png" alt="Imagen de lentes" width="100%">
+                    <h2 class="letra" id="tamanio">SPECTRA ROAD METAL</h2>
+                    <p class="alinear_texto">$ 4200</p>
+                </a>
+            </div>
+        
+            <div class="target">
+                <a href="">
+                    <img class="target_img centrar_img" src="img/Imagen_lentes_2.png" alt="Imagen de lentes" width="100%">
+                    <h2 class="letra" id="tamanio">WAYFARER OVAL CHANGE</h2>
+                    <p class="alinear_texto">$ 3299</p>
+                </a>
+            </div>
+        
+            <div class="target">
+                <a href="">
+                    <img class="target_img centrar_img" src="img/Imagen_lentes_3.png" alt="Imagen de lentes" width="100%">
+                    <h2 class="letra" id="tamanio">LOOK BASE-HOT</h2>
+                    <p class="alinear_texto">$ 3200</p>
+                </a>
+            </div>
+        
+            <div class="target">
+                <a href="">
+                    <img class="target_img centrar_img" src="img/Imagen_lentes_4.png" alt="Imagen de lentes" width="100%">
+                    <h2 class="letra" id="tamanio">X8 SPECTRA MODERN</h2>
+                    <p class="alinear_texto">$ 2299</p>
+                </a>
+            </div>
+        
+            <div class="target">
+                <a href="">
+                    <img class="target_img centrar_img" src="img/Imagen_lentes_5.png" alt="Imagen de lentes" width="100%">
+                    <h2 class="letra" id="tamanio">WAYFARER ONE</h2>
+                    <p class="alinear_texto">$ 4499</p>
+                </a>
+            </div>
+        
+            <div class="target">
+                <a href="">
+                    <img class="target_img centrar_img" src="img/Imagen_lentes_3.png" alt="Imagen de lentes" width="100%">
+                    <h2 class="letra" id="tamanio">LOOK BASE-HOT</h2>
+                    <p class="alinear_texto">$ 3200</p>
+                </a>
+            </div>
+        
+            <div class="target">
+                <a href="">
+                    <img class="target_img centrar_img" src="img/Imagen_lentes_4.png" alt="Imagen de lentes" width="100%">
+                    <h2 class="letra" id="tamanio">X8 SPECTRA MODERN</h2>
+                    <p class="alinear_texto">$ 2299</p>
+                </a>
+            </div>
+        </nav>
